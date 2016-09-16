6502abcf73fa7b1ed7f5c9af8dae5d1c8088f969
Frederik Krautwald <fkrautwald@gmail.com>
2016-09-16 13:59:19 +0200

    chore(): install underscore theme

---
+++ README.md
@@ -0,0 +1,3 @@
+# WordPress Theme for Slaegten Fisker
+
+WordPress theme based on the underscores theme.
---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Slaegten_Fisker
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'slaegtenfisker' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'slaegtenfisker' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'slaegtenfisker' ), 'slaegtenfisker', '<a href="https://github.com/Frikki" rel="designer">Frederik Krautwald</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
